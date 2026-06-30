f4e099547032392f4ceb572fca207abc056465af
Kirill Sky <109349859+krlskyy@users.noreply.github.com>
2026-06-30 09:57:39 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+export const dynamic = 'force-dynamic';
+
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <body className="bg-gray-50 text-gray-900">{children}</body>
+      <body className="bg-[#050505] text-zinc-200">{children}</body>
     </html>
   );
 }
