2b6bc135ebcfad8ef7c35cde3bd6d5b3ec81f183
Kirill Sky <109349859+krlskyy@users.noreply.github.com>
2026-06-30 10:05:15 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export const dynamic = 'force-dynamic';
-
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <body className="bg-[#050505] text-zinc-200">{children}</body>
+      <head>
+        {/* Это гарантирует, что стили подтянутся */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className="bg-[#050505] text-zinc-200">
+        {children}
+      </body>
     </html>
   );
 }
